5acdebff762242804d8952aa2b46e56ad3418583
David <dkoo15@gmail.com>
2019-05-15 18:37:53 -0400

    chanaged parameter names and contour settings

--- load_parameters.py
+++ load_parameters.py
@@ -21,10 +21,12 @@ cg = [1.2077, 0, 0.6452]
 span_stations = [0.0235, 0.267, 0.557, 0.695, 0.828, 0.944]
 
 # Indicate which y/b is where the winglet starts. Set >1.0 for no winglet
-dihedral_stn = 1.1
+winglet_y = 1.1
 
 # Indicate which y/b is where the wing starts
 wing_offset = 0.0
 
 wing = Component("Wing")
 components = [wing]
+
+dihedral = 0.0
